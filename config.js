c372c56ecc5bf838c3bea8e260d469cadba8c0ba
Safirino <68868229+Safirino@users.noreply.github.com>
2025-04-29 16:51:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
         buyer: '1014861875894300775', // Buyer
-        couleur: 'bf1313', // Couleur HEX
+        couleur: '#bf1313', // Couleur HEX
         footer: 'Safou Protect', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
